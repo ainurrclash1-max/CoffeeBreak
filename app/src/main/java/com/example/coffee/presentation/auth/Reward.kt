82033c839c11e1f41cd9ce8398c386ba6e28e9dd
pc4 <ainurrclash1@gmail.com>
2026-02-18 14:06:16 +0300

    Sprint 4

--- app/src/main/java/com/example/coffee/presentation/auth/Reward.kt
+++ app/src/main/java/com/example/coffee/presentation/auth/Reward.kt
@@ -88,4 +88,3 @@ fun RewardScreen(){
             }
         }
     }
-}
--- app/src/main/java/com/example/coffee/ui/theme/PoppinsFontFamily.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coffee.ui.theme
-
-import android.R.attr.fontWeight
-import android.graphics.fonts.Font
-import androidx.compose.ui.text.font.FontFamily
-
